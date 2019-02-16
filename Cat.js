091f8df2534c24c05ef2ff8fb278e9e275890e20
Brandon <brandonl271@gmail.com>
2019-02-16 15:35:37 -0800

    added cat class

---
+++ Cat.js
@@ -0,0 +1,11 @@
+import Mammal from Mammal.js
+class Cat extends Mammal{
+    constructor(name,sex,age){
+        super();
+        this.name=name;
+        this.sex=sex;
+        this.age=age;
+    }
+}
+
+export Cat
