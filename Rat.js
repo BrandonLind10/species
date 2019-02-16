4b8909efd9a0a9d7b0ff3a3ff4f8a6762dca8ce0
Brandon <brandonl271@gmail.com>
2019-02-16 15:52:21 -0800

    added rat class

---
+++ Rat.js
@@ -0,0 +1,11 @@
+import Mammal from Mammal.js
+class Rat extends Mammal{
+    constructor(name,sex,age){
+        super();
+        this.name=name;
+        this.sex=sex;
+        this.age=age;
+    }
+}
+
+export Rat
