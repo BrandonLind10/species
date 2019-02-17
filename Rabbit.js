034b00a3dc2cf5e0f09fe9e1c4361fa5b9a50058
BrandonLind10 <45861283+BrandonLind10@users.noreply.github.com>
2019-02-16 17:58:30 -0800

    Create Rabbit.js
    created rabbit class

---
+++ Rabbit.js
@@ -0,0 +1,12 @@
+import Mammal from Mammal.js
+
+class Rabbit extend Mammal{
+  constructor(name,sex,age){
+    super();
+    this.name=name;
+    this.sex=sex;
+    this.age=age;
+  }
+}
+
+export Rabbit
