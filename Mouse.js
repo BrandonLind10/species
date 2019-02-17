d9081dbd6567dd49cc474eda4d89abf5a5718586
Brandon <brandonl271@gmail.com>
2019-02-16 16:00:24 -0800

    added mouse class

---
+++ Mouse.js
@@ -0,0 +1,11 @@
+import Mammal from Mammal.js
+class Mouse extends Mammal{
+    constructor(name,sex,age){
+        super();
+        this.name=name;
+        this.sex=sex;
+        this.age=age;
+    }
+}
+
+export Mouse
